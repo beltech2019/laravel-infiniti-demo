15e0f0a0ab2862a8ce72fa67dd9bb4b356f2659b
saurabh-bel <saurabh@bel-technology.com>
2025-08-22 17:44:30 +0530

    change

--- app/Models/GameArt.php
+++ app/Models/GameArt.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class GameArt extends Model
 {
-    protected $table = 'stpl_game_info';
+    protected $table = 'stpl_gameart_info';
 
     // If the table doesn't have timestamps
     public $timestamps = false;
