90877ff11289f1cda436747e51bbf5b8c8341f38
sanjeet.singh <sanjeetsingh856180@gmail.com>
2025-09-08 11:02:05 +0530

    create migration model

---
+++ app/Models/FAQ.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class FAQ extends Model
+{
+
+    use HasFactory, SoftDeletes;
+
+
+    protected $table = 'tb_faq';
+
+    protected $fillable = [
+        'question',
+        'answer',
+    ];
+}
---
+++ database/migrations/2025_09_08_045024_create_faq_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tb_faq', function (Blueprint $table) {
+            $table->id();
+            $table->text('question');
+            $table->text('answer')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tb_faq');
+    }
+};
