5323a2f6bdfe131274d05b2f535cbcfb30e4dcee
saurabh-bel <saurabh@bel-technology.com>
2025-09-15 17:30:17 +0530

    changes

--- resources/views/admin/articles/index.blade.php
+++ resources/views/admin/articles/index.blade.php
@@ -9,6 +9,7 @@
                     <th>ID</th>
                     <th>Name</th>
                     <th>Publish</th>
+                    <th>Action</th>
                 </tr>
             </thead>
             <tbody>
@@ -23,6 +24,23 @@
                                 {{ $article->publish ? 'Published' : 'Unpublished' }}
                             </button>
                         </form>
+                    </td>
+                    <td>
+                        @if($article->name == 'Responsible Gaming')
+                            <a href="{{ route('admin.responsibleGamingConfig') }}" class="{{ request()->routeIs('admin.responsibleGamingConfig') ? 'active' : '' }}">
+                                <i class="bi bi-pencil-square"></i>
+                            </a>
+                        @endif
+                        @if($article->name == 'Tearms & Conditions')
+                            <a href="{{ route('admin.termsandconditionConfig') }}" class="{{ request()->routeIs('admin.termsandconditionConfig') ? 'active' : '' }}">
+                                <i class="bi bi-pencil-square"></i>
+                            </a>
+                        @endif
+                        @if($article->name == 'Privacy Policy')
+                            <a href="{{ route('admin.privacyPolicyConfig') }}" class="{{ request()->routeIs('admin.privacyPolicyConfig') ? 'active' : '' }}">
+                                <i class="bi bi-pencil-square"></i>
+                            </a>
+                        @endif
                     </td>
                 </tr>
                 @endforeach
